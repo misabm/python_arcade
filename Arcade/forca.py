7933f5294f43279c2ee14c288b43fff9f75ed44c
AltemirFilho <filho.altemircds@gmail.com>
2026-04-16 21:52:43 -0300

    Jogo da forca finalizado

--- Arcade/forca.py
+++ Arcade/forca.py
@@ -1,4 +1,65 @@
-def jogar():
-    print("\n[JOGO EM CONSTRUÇÃO]")
-    print("Você ganhou 10 pontos de consolação!")
-    return 10 # Retorna 10 pontos para testarmos se o menu está a somar corretamente
+import random 
+
+def jogar_forca():
+    print("="*40)
+    print("BEM-VINDO AO JOGO DA FORCA!")
+    print("="*40)
+
+    banco_tematico = {
+        "Animal": ["CACHORRO", "ELEFANTE", "GIRAFA", "MACACO", "TIGRE", "JACARE"],
+        "Objeto": ["CADEIRA", "TECLADO", "TELEFONE", "RELOGIO", "MOCHILA", "GARRAFA"],
+        "Profissão": ["MEDICO", "ENGENHEIRO", "PROFESSOR", "BOMBEIRO", "ADVOGADO", "PROGRAMADOR"],
+        "Fruta": ["BANANA", "MORANGO", "ABACAXI", "LARANJA", "MELANCIA", "PITAYA"],
+        "Ação": ["CORRER", "PULAR", "NADAR", "ESTUDAR", "TRABALHAR", "DORMIR"]
+    }
+
+    temas_disponiveis = list(banco_tematico.keys())
+
+    tema_sorteado = random.choice(temas_disponiveis)
+    
+    palavra_oculta = random.choice(banco_tematico[tema_sorteado])
+    
+    letras_chutadas = [] 
+    
+    tracinhos = ["_"] * len(palavra_oculta) 
+    
+    vidas = 6 
+    
+    print(f"\nPREPARE-SE! O tema sorteado foi: >>> {tema_sorteado.upper()} <<<")
+    print(f"A palavra tem {len(palavra_oculta)} letras.")
+    
+    while vidas > 0 and "_" in tracinhos:
+        print(f"\nPalavra: {' '.join(tracinhos)}")
+        print(f"Tentativas: {', '.join(letras_chutadas)}")
+        print(f"Vidas restantes: {vidas}")
+        chute = input("Digite uma letra: ").upper().strip()
+
+        if len(chute) != 1 or not chute.isalpha():
+            print("Entrada inválida! Digite apenas uma letra do alfabeto.")
+            continue 
+
+        if chute in letras_chutadas:
+            print("Você já tentou essa letra! Escolha outra.")
+            continue
+        
+        letras_chutadas.append(chute)
+
+        if chute in palavra_oculta:
+            print("Acertou a letra!")
+            
+            for indice, letra in enumerate(palavra_oculta):
+                if letra == chute:
+                    tracinhos[indice] = chute
+        else:
+            print("Letra incorreta!")
+            vidas -= 1 
+
+    print("\n" + "="*40)
+    
+    if "_" not in tracinhos:
+        print(f"PARABÉNS! Você venceu! A palavra era: {palavra_oculta}")
+    else:
+        print(f"FIM DE JOGO! Suas vidas acabaram. A palavra era: {palavra_oculta}")
+
+if __name__ == "__main__":
+    jogar_forca()
